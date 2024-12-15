65a015127858335a8ba1228c9de4a5b1feb8fa5f
andreyk1n <betas23wr@gmail.com>
2024-12-15 12:38:02 +0200

    add header

--- js/functions/scrollHeader.js
+++ js/functions/scrollHeader.js
@@ -3,15 +3,19 @@
 // саме для хедера
 // ---------------------------------------------------------------------------------------------------------------
 export function initScrollControlledHeader() {
-
   let lastScrollTop = 0;
+  const header = document.querySelector('.header');
+
   window.addEventListener('scroll', () => {
     const scrollTop = window.pageYOffset;
-    document.querySelector('.header').style.top = (scrollTop <= 10 || scrollTop < lastScrollTop) ? '0' : '-100px';
-    lastScrollTop = scrollTop;
+    // add class
+    header.classList.toggle('scroll-header', scrollTop > 10);
+    // hide when scroll
+    // header.style.top = (scrollTop <= 10 || scrollTop < lastScrollTop) ? '0' : '-100px';
+    // lastScrollTop = scrollTop;
   });
-
 }
+
 // ---------------------------------------------------------------------------------------------------------------
 
 
